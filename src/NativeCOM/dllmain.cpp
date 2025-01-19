f78fcb288d99ba3fd88c3977134c4bc20187456f
Adam Pluciński <adaskothebeast@gmail.com>
2025-01-19 19:27:09 +0100

    fix code smells

--- src/NativeCOM/dllmain.cpp
+++ src/NativeCOM/dllmain.cpp
@@ -11,6 +11,5 @@ CNativeCOMModule _AtlModule;
 // DLL Entry Point
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
-	hInstance;
 	return _AtlModule.DllMain(dwReason, lpReserved);
 }
